3005c0ff3089bc702add639661fc5c566c187bb0
pachevang <vangpache@gmail.com>
2020-01-21 21:45:57 -0600

    adding flexbox styling to complete Blog layout

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -19,12 +19,14 @@ class Blog extends Component {
         return (
             <div className="blog-container">
                 <div className="left-blog-container">
-                    <p>HELLO. this is the featured blog div.</p>
+                    <div>
+                        <p>HELLO. this is the featured blog div.</p>
+                    </div>
                 </div>
                 
 
                 <div className="right-blog-container">
-                    <p>this is the right side.</p>
+                    {/* <p>this is the right side.</p> */}
                     <div className="blog-first-row">
                         <div className="blogs-row-one">
                             <p>blog 1</p>
@@ -39,12 +41,31 @@ class Blog extends Component {
 
 
                     <div className="blog-second-row-container">
+                        <div className="blog-blockcontainer">
                         <div className="blog-second-row-thirds-container">
-                            
-                            
+                            <div className="blog-thirds">
+                                <p>blog 4</p>
+                            </div>
+                            <div className="blog-thirds">
+                                <p>blog 5</p>
+                            </div>
                         </div>
 
+                        <div className="blog-thirds-bottom">
+                            <div>
+                                <p>blog 6</p>
+                            </div>
+                        </div>
+                        </div>
+
+
+
+
+
                         <div className="blog-second-row-single">
+                            <div>
+                                <p>blog 7</p>
+                            </div>
 
                         </div>
 
